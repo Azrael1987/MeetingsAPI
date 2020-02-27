ec506a7711293bc872cd72766c97838cdd848c91
Adam <gruszecki.adam@gmail.com>
2020-02-28 00:23:10 +0100

    podstawowe klasy, context i 1 migracja do bazy danych

---
+++ MeetingsAPI/Enitities/MeetupContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MeetingsAPI.Enitities
+{
+    public class MeetupContext : DbContext
+    {
+        private string _connectionString = "Server=DESKTOP-MSTE5ED\\SQLEXPRESS;Database=MeetingsDb;Trusted_Connection=True;";
+
+        public DbSet<Meetup> Meetups { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Meetup>()
+                .HasOne(m => m.Location)
+                .WithOne(l => l.Meetup)
+                .HasForeignKey<Location>(l => l.MeetupId);
+
+            modelBuilder.Entity<Meetup>()
+                .HasMany(m => m.Lectures)
+                .WithOne(l => l.Meetup);
+        }
+
+        protected   override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
