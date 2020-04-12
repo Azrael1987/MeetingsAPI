9a8b0b689d575e91e454148dd5ca2591d344078c
Azrael1987 <gruszecki.adam@gmail.com>
2020-04-12 18:59:30 +0200

    prosta konfiguracja swaggera

--- MeetingsAPI/Startup.cs
+++ MeetingsAPI/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using MeetingsAPI.Enitities;
 using MeetingsAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace MeetingsAPI
 {
@@ -34,11 +28,21 @@ namespace MeetingsAPI
             services.AddScoped<IMeetupService, MeetupService>();
             services.AddScoped<ILectureService, LectureService>();
             services.AddAutoMapper(this.GetType().Assembly);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo() { Title = "MeetingsAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, MeetupSeeder meetupSeeder)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MeetingsAPI v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
